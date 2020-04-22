0fa816facc314c08c023da4c7379756c4b66abc8
Kevin Chen <kevc528@seas.upenn.edu>
2020-04-22 02:00:30 -0500

    finish about page and templates

--- PersonalSite/PersonalSite/settings.py
+++ PersonalSite/PersonalSite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'contact',
+    'about',
+    'projects'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,9 @@ ROOT_URLCONF = 'PersonalSite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- PersonalSite/PersonalSite/urls.py
+++ PersonalSite/PersonalSite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from about.views import about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', about, name='about')
 ]
