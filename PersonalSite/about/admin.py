c462ba1dbee3f89de5e3f82c1bf72bee6a3adc2a
Kevin Chen <kevc528@seas.upenn.edu>
2020-07-13 22:18:14 -0500

    clear django files

--- PersonalSite/about/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from projects.models import ProjectCategory, Comment, Project
-
-# Register your models here.
-admin.site.register(ProjectCategory)
-admin.site.register(Comment)
-admin.site.register(Project)
--- PersonalSite/contact/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-# render the contacts page
-def contact(request):
-    return render(request, 'contact.html', {})
