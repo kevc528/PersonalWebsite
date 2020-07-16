7dc1e57249a84a32221731adc40a8f846591c7fe
Kevin Chen <kevc528@seas.upenn.edu>
2020-07-15 19:36:23 -0500

    change route logic

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,19 @@ function loadFooter() {
 function downloadResume() {
   window.open('resume.pdf');
 }
+
+function innerLoadNav() {
+  $("#nav-placeholder").load("../nav.html");
+  $("#home-link").attr("href", "../");
+  $("#projects-link").attr("href", "../projects");
+  $("#contact-link").attr("href", "../contact");
+  $("#download-button").attr("onclick", "innerDownloadResume()");
+}
+
+function innerLoadFooter() {
+  $("#footer-placeholder").load("../footer.html");
+}
+
+function innerDownloadResume() {
+  window.open('../resume.pdf');
+}
